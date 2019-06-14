5f6a50f12d4968515f3eba22160402ff0ab52d8f
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-06-14 20:51:51 +0530

    Update primme.cpp

--- primme.cpp
+++ primme.cpp
@@ -1,19 +1,18 @@
 #include<iostream>
-#include<math.h>
 using namespace std;
 int main(){
 int a,b,fac;
 cin>>a;
 cin>>b;
-for(int j=a;j<=b;j++){
+for(int j=a;j<b;j++){
         fac=1;
-for(int i=2;i<=j-1;i++){
+for(int i=2;i<=j-1;i++){              
     if(j%i==0){
         fac=0;
         break;}
 }
-if(fac==1){if(j!=1){cout<<j;}}
+if(fac==1){if(j!=1){cout<<j<<" ";}}
 }
 return 0;
 }
-
+}
