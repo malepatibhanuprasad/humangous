2bc419d1cd670bbb1057d380b2b169622c0518e1
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-06-17 09:22:20 +0530

    Create idex.cpp

---
+++ idex.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+        int n,i,a[100],temp=0,m;
+    cin>>n;
+    for(i=0;i<n;i++){cin>>a[i];}
+    for(i=0;i<n;i++){
+    cout<<a[i]<<" "<<i<<endl;
+    }
+    return 0;}
