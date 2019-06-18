bb9779ca3b4db1a537899a5f5f08818808e65242
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-06-18 13:14:39 +0530

    Create str.cpp

---
+++ str.cpp
@@ -0,0 +1,15 @@
+}*/#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main(){
+    char s[100];
+    int a,c=0,i;
+    gets(s);
+    a=strlen(s);
+    for(i=0;s[i];i++){
+    if(s[i]==' ')
+        c+=1;
+}
+    printf("%d",a-c);
+return 0;
+}
