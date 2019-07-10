81b35733251c67dc97eb7b08ffd56b73ce5c9ec6
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-07-10 18:24:55 +0530

    Create wihtocat.c

---
+++ wihtocat.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char a[20],b[20];
+gets(a);
+gets(b);
+int n1=strlen(a);
+int n2=strlen(b);
+char c[n1+n2-1];
+int i,j,k=0;
+for(i=0;i<n1;i++){
+    if(a[i]!='\0'){
+        c[i]=a[i];
+    }
+}
+printf("%d\n%d\n",i,n2);
+for(j=i;j<i+n2;j++){
+    if(b[k]!='\0'){
+        c[j]=b[k];
+        k++;
+    }
+    }
+printf("%s",c);
+return 0;
+}
