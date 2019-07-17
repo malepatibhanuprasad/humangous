5c20882b5ca8bc2331ac30b869701b7055be0b53
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-07-17 09:04:07 +0530

    Update hzero.c

--- hzero.c
+++ hzero.c
@@ -8,7 +8,7 @@ for(i=0;i<n;i++)
 for(i=0;i<n;i++){
     for(j=i+1;j<n;j++){
         if((abs(a[i]+a[j]))<min){
-            min=abs(a[i]+a[j];
+            min=abs(a[i]+a[j]);
             c=i;
             d=j;
          }
