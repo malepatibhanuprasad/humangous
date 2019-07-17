ac9c79ba9e37207b9c96cb6c2b7a7a8cea68c83d
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-07-17 14:44:25 +0530

    Create fstrepetdele.c

---
+++ fstrepetdele.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int n,i,j;
+scanf("%d",&n);
+int a[n],min=n+1;
+for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+for(i=0;i<n;i++){
+    for(j=i+1;j<n;j++){
+        if(a[i]==a[j]){
+            if(min>j)
+                min=j;
+        }
+    }
+}
+if(min>=0)
+    printf("%d",a[min]);
+else
+    printf("unique");
+return 0;
+}
