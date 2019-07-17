cec867612c26a5e346ddb9c05ab1a6f5df794f7d
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-07-17 09:00:34 +0530

    Update hzero.c

--- hzero.c
+++ hzero.c
@@ -2,17 +2,16 @@
 int main(){
 int n,m,i,j;
 scanf("%d",&n);
-int a[n],s=0,c,d;
+int a[n],s=0,c,d,min=1000;
 for(i=0;i<n;i++)
     scanf("%d",&a[i]);
 for(i=0;i<n;i++){
-    for(j=0;j<n;j++){
-        if((a[i]+a[j])==0){
+    for(j=i+1;j<n;j++){
+        if((abs(a[i]+a[j]))<min){
+            min=abs(a[i]+a[j];
             c=i;
             d=j;
-            break;
-        }
-        break;
+         }
     }
 }
 printf("%d %d",a[c],a[d]);
