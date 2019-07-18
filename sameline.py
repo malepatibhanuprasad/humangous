4fe8f497c070e05dfda3443c60ec855747f81f99
malepatibhanuprasad <49300472+malepatibhanuprasad@users.noreply.github.com>
2019-07-18 18:20:20 +0530

    Create sameline.py

---
+++ sameline.py
@@ -0,0 +1,19 @@
+p1=int(input())
+p2=int(input())
+p3=int(input())
+p4=int(input())
+p5=int(input())
+p6=int(input())
+if(p1==p3):
+    if(p3==p5):
+        print ("yes")
+    else:
+        print("no")
+else:
+    if(p2==p4):
+       if(p4==p6):
+           print ("yes")
+       else:
+           print ("no")
+    else:
+        print("no")
